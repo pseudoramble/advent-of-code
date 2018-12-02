2d9ec71823a442a410bf1fab405118d147351c89
Doug Swain <pseudoramble@gmail.com>
2018-12-02 10:50:26 -0500

    Hacky solution to 2018 Day 1.

--- 2018/AdventOfCode2018/Day01.fs
+++ 2018/AdventOfCode2018/Day01.fs
@@ -1,4 +1,27 @@
 namespace AdventOfCode2018
 
 module Day01 =
-  let test a = a * a;
+  let cycle (values: seq<int>) =
+    Seq.initInfinite (fun _ -> values) |> Seq.concat
+
+  let frequency (values: seq<int>) =
+    Seq.sum values
+
+  let repeat (values: seq<int>) =
+    let mutable foundCycles = Set.empty
+    let mutable acc = 0
+
+    let results = 
+      values 
+      |> cycle
+      |> Seq.skipWhile (fun value -> 
+          acc <- acc + value
+          match Set.contains acc foundCycles with
+          | true -> false
+          | false -> 
+            foundCycles <- Set.add acc foundCycles;
+            true;
+        )
+
+    printfn "%A" results
+    acc
