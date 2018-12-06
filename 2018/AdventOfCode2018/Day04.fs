626df94e7ebd71e31cf974352c92693bbf2f7bfe
Doug Swain <pseudoramble@gmail.com>
2018-12-05 20:14:53 -0500

    Solution for Day 4, 2018.

---
+++ 2018/AdventOfCode2018/Day04.fs
@@ -0,0 +1,105 @@
+namespace AdventOfCode2018
+
+open System
+open System.Text.RegularExpressions
+open System.Globalization
+
+module Guard = 
+  type GuardAction =
+    BeginShift of string
+    | FallAsleep
+    | WakeUp
+
+  type GuardLog = {
+    action: GuardAction
+    dateTime: DateTime;
+  }
+
+  type Guard = {
+    id: string;
+    logs: GuardLog list
+  }
+  
+  let addAction action guard =
+    {
+      Guard.id=guard.id
+      logs=List.rev (action :: List.rev guard.logs)
+    }
+
+  let sleepySchedule guard = 
+    let timeline = Array.zeroCreate 60
+    Seq.toList guard.logs
+    |> List.chunkBySize 2
+    |> Seq.iter (fun [a; b] ->
+      let minuteRange = [a.dateTime.Minute .. b.dateTime.Minute]
+      Seq.iter (fun minute -> 
+        Array.set timeline minute ((Array.get timeline minute) + 1)
+      ) minuteRange
+    )
+    Array.indexed timeline
+
+  let sleepiestMinute guard =
+    let indexedTimeline = sleepySchedule guard
+    let mostSleepy = indexedTimeline |> Array.maxBy snd |> snd
+    Array.filter (fun (i, min) -> min = mostSleepy) indexedTimeline
+
+  let sleepySchedules guards =
+    Seq.map (fun g -> (g.id, sleepySchedule g)) guards
+
+  let guardSleepingTotals guards =
+    Seq.map (fun guard ->
+      let sleepyMinutes =
+        Seq.toList guard.logs
+        |> List.chunkBySize 2
+        |> Seq.sumBy (fun [a; b] ->
+          b.dateTime.Minute - a.dateTime.Minute
+        )
+      (guard, sleepyMinutes)
+    ) guards
+
+  let sleepiestGuard guards = 
+    guardSleepingTotals guards
+    |> Seq.maxBy snd
+    
+module Day04 =
+  open Guard
+
+  let guardLogPattern = "\[(.*)\] (.*)"
+  let guardPattern = "Guard #([0-9]+)"
+
+  let guardOfGuardLog log = 
+    let matcher = Regex.Match(log, guardPattern)
+    {
+      id=matcher.Groups.Item(1).Value
+      logs=[]
+    }  
+
+  let guardsOfGuardLogs logs =
+    Seq.fold (fun (guards, currentGuard) guardLog ->
+      match guardLog.action with
+      | FallAsleep | WakeUp -> 
+        let updatedGuard = Guard.addAction guardLog (Option.get currentGuard)
+        (Map.add updatedGuard.id updatedGuard guards, Some updatedGuard)
+      | BeginShift(entry) as bs -> 
+        let guard = guardOfGuardLog entry
+        match Map.containsKey guard.id guards with
+        | true -> (guards, Some <| Map.find guard.id guards)
+        | false -> (Map.add guard.id guard guards, Some guard)
+    ) (Map.empty, None) logs
+    |> fst
+    |> Map.toSeq
+    |> Seq.map snd
+
+  let parseGuardLog log =
+    let matcher = Regex.Match(log, guardLogPattern)
+    let dateTime = DateTime.Parse(matcher.Groups.Item(1).Value)
+    let action = 
+      match (matcher.Groups.Item(2).Value) with
+      "falls asleep" -> GuardAction.FallAsleep
+      | "wakes up" -> GuardAction.WakeUp
+      | msg -> GuardAction.BeginShift(msg)
+
+    {
+      action=action
+      dateTime=dateTime;
+    }
--- 2018/AdventOfCode2018/Script.fsx
+++ 2018/AdventOfCode2018/Script.fsx
@@ -1,6 +1,7 @@
 #load "Day01.fs"
 #load "Day02.fs"
 #load "Day03.fs"
+#load "Day04.fs"
 #load "Logger.fs"
 
 open System.IO
@@ -8,6 +9,7 @@ open AdventOfCode2018
 open AdventOfCode2018.Day01
 open AdventOfCode2018.Day02
 open AdventOfCode2018.Day03
+open AdventOfCode2018.Day04
 
 let dayOne () =
   let logger = new LoggingBuilder("Day One\n-------------")
@@ -42,6 +44,25 @@ let dayThree () =
     return overlapArea inputs
   }
 
+let dayFour () =
+  let logger = new LoggingBuilder("Day Four\n--------------")
+  let inputs = 
+    Seq.map parseGuardLog <| File.ReadAllLines "./2018/AdventOfCode2018/inputs/day-04"
+    |> Seq.sortBy (fun log -> log.dateTime)
+  let guards = guardsOfGuardLogs inputs
+
+  let sleepiestGuard = Guard.sleepiestGuard guards
+  let sleepySchedules = 
+    Guard.sleepySchedules guards
+    |> Seq.map (fun (id, schedule) -> (id, Seq.maxBy snd schedule))
+  let maxMinuteSlept = Seq.maxBy (fun (_, minute) -> snd minute) sleepySchedules
+
+  let partOne = (sleepiestGuard, sleepiestGuard |> fst |> Guard.sleepiestMinute)
+  let partTwo = Seq.filter (fun (_, minute) -> minute = snd maxMinuteSlept) sleepySchedules
+
+  (partOne, partTwo)
+ 
 // dayOne ()
 // dayTwo ()
-dayThree ()
+// dayThree ()
+dayFour () 
