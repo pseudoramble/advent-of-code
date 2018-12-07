512f0b03319855126e0ec1d320a4434638af05d1
Doug Swain <pseudoramble@gmail.com>
2018-12-06 21:15:52 -0500

    WIP: Got sample input functioning. Still working on actual input.

--- 2018/AdventOfCode2018/Day04.fs
+++ 2018/AdventOfCode2018/Day04.fs
@@ -4,8 +4,7 @@ open System
 open System.Text.RegularExpressions
 open System.Globalization
 
-
-module Day04 =
+module Guard = 
   type GuardAction =
     BeginShift of string
     | FallAsleep
@@ -16,7 +15,70 @@ module Day04 =
     dateTime: DateTime;
   }
 
+  type Guard = {
+    id: string;
+    logs: GuardLog list
+  }
+  
+  let addAction action guard =
+    {
+      Guard.id=guard.id
+      logs=List.rev (action :: List.rev guard.logs)
+    }
+
+  let sleepiestMinute guard =
+    let timeline = Array.zeroCreate 60
+    Seq.toList guard.logs
+    |> List.chunkBySize 2
+    |> Seq.iter (fun [a; b] ->
+      let minuteRange = [a.dateTime.Minute .. b.dateTime.Minute]
+      printfn "%A" minuteRange
+      Seq.iter (fun minute -> 
+        Array.set timeline minute ((Array.get timeline minute) + 1)
+      ) minuteRange
+    )
+    Array.indexed timeline |> Array.maxBy snd
+
+  let sleepiestGuard guards =
+    Seq.map (fun guard ->
+      let sleepyMinutes =
+        Seq.toList guard.logs
+        |> List.chunkBySize 2
+        |> Seq.sumBy (fun [a; b] ->
+          b.dateTime.Minute - a.dateTime.Minute
+        )
+      (guard, sleepyMinutes)
+    ) guards
+    |> Seq.maxBy snd
+    
+module Day04 =
+  open Guard
+
   let guardLogPattern = "\[(.*)\] (.*)"
+  let guardPattern = "Guard #([0-9]+)"
+
+  let guardOfGuardLog log = 
+    let matcher = Regex.Match(log, guardPattern)
+    {
+      id=matcher.Groups.Item(1).Value
+      logs=[]
+    }  
+
+  let guardsOfGuardLogs logs =
+    Seq.fold (fun (guards, currentGuard) guardLog ->
+      match guardLog.action with
+      | FallAsleep | WakeUp -> 
+        let updatedGuard = Guard.addAction guardLog (Option.get currentGuard)
+        (Map.add updatedGuard.id updatedGuard guards, Some updatedGuard)
+      | BeginShift(entry) as bs -> 
+        let guard = guardOfGuardLog entry
+        match Map.containsKey guard.id guards with
+        | true -> (guards, Some <| Map.find guard.id guards)
+        | false -> (Map.add guard.id guard guards, Some guard)
+    ) (Map.empty, None) logs
+    |> fst
+    |> Map.toSeq
+    |> Seq.map snd
 
   let parseGuardLog log =
     let matcher = Regex.Match(log, guardLogPattern)
@@ -31,6 +93,3 @@ module Day04 =
       action=action
       dateTime=dateTime;
     }
-
-  let badGuardStats guardLog =
-    ()
